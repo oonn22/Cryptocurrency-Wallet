45da565ebf780e557ad633daa81979b8869784a0
oonn22 <dropthepuck12@gmail.com>
2021-12-12 20:41:24 -0500

    added more tests and commented purpose

--- Crypto/Tests.js
+++ Crypto/Tests.js
@@ -1,14 +1,21 @@
-const Crypto = require('./Crypto.js');
+const Crypto = require("./Crypto.js");
 
+//Simple file to test if various functions can be performed.
+
+let testString = "Hello World!";
+let testPassword = "1234";
 let msgBytes = Crypto.randomBytes(64);
 let hash = Crypto.hash(msgBytes);
 let secret = Crypto.newSecret();
 let pub = Crypto.getPublic(secret);
 let sig = Crypto.signMessage(hash, secret);
 let valid = Crypto.verifySignature(hash, sig, pub);
+let enc = Crypto.encrypt(Buffer.from(testString), testPassword);
+let dec = Crypto.decrypt(enc, testPassword);
 
 console.log(Crypto.encode(hash));
 console.log(pub);
 console.log(Crypto.encode(sig));
 console.log(valid);
-
+console.log(enc);
+console.log(Buffer.from(dec).toString());
