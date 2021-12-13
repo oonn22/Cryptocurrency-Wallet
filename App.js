5cc04883d175ef17ab922521a8c402671ef71cdc
oonn22 <dropthepuck12@gmail.com>
2021-12-12 20:03:20 -0500

    Added files not updated in initial push

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Toast from "react-native-toast-notifications";
+
+import StartScreen from "./Screens/StartScreen";
+import WalletScreen from "./Screens/WalletScreen";
+import ImportScreen from "./Screens/ImportScreen";
+import CreateWalletScreen from "./Screens/CreateWalletScreen";
+import SettingsScreen from "./Screens/SettingsScreen";
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Start"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="WalletScreens" component={WalletScreens} />
+          <Stack.Screen name="Start" component={StartScreen} />
+          <Stack.Screen name="Import" component={ImportScreen} />
+          <Stack.Screen name="CreateWallet" component={CreateWalletScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <Toast ref={(ref) => (global["Toast"] = ref)} />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function WalletScreens() {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Wallet"
+      screenOptions={{ headerShown: false }}
+    >
+      <Drawer.Screen name="Wallet" component={WalletScreen} />
+      <Drawer.Screen name="Settings" component={SettingsScreen} />
+    </Drawer.Navigator>
+  );
+}
+
+//TODO left off creating wallet class and trying to get working node to know whet return values look like.
+
+//TODO https://docs.expo.dev/versions/latest/sdk/async-storage/ store wallets
+
+// s KCUYX726TM5F6ZWI2JOT2WEIFAMND2TECIT6HU4TZQM2O3XO6WRQ
+// a DSFLYXMK46AEVZWS5VQ5KWMYTM3MMYTCKW7FG4WIKBPUQ2DMKY5Q
